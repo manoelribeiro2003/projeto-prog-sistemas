0b6f09c7a381a85adb09b1d69d0f95501cf6764d
ribeiromanoel2003@gmail.com <maureck1977@gmail.com>
2024-04-27 09:52:27 -0300

    pull

---
+++ Peojeto-Prog-Sistem/StatusPatrimonio.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Peojeto_Prog_Sistem
+{
+    public partial class StatusPatrimonio : Form
+    {
+        public StatusPatrimonio()
+        {
+            InitializeComponent();
+
+        }
+
+        private void StatusPatrimonio_Load(object sender, EventArgs e)
+        {
+            //Usando apenas DataTable (sem uso de List<>)
+            DataTable listStatus = Banco.buscarListStatusPatri();
+            foreach (DataRow item in listStatus.Rows)
+            {
+                lbxStatusPatri.Items.Add(item[0].ToString());
+            }
+
+
+
+            //Usando DataTable com auxilio de List<>
+            /*List<string> listStatus = Banco.buscarListStatusPatri();
+            foreach (string item in listStatus)
+            {
+                lbxStatusPatri.Items.Add(item.ToString());
+            }*/
+        }
+    }
+}
